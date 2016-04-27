df94546fa9c0e9992bf597dba41326e1201126ea
Ash <ash@ashryanbeats.com>
2016-04-26 20:37:40 -0400

    Reading from MTA on launch.

---
+++ app.js
@@ -0,0 +1,20 @@
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+var apiKeys = require('./config');
+
+var requestSettings = {
+  method: 'GET',
+  url: 'http://datamine.mta.info/mta_esi.php?key=' + apiKeys.mta,
+  encoding: null
+};
+
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+    feed.entity.forEach(function(entity) {
+      if (entity.trip_update) {
+        console.log(entity.trip_update);
+      }
+    });
+  }
+});
